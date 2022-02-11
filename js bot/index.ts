6f302a8af251cd8157fc04efc2396789492c7c47
forsakenMystery <hkhashechi@yahoo.com>
2022-02-11 22:50:43 +0330

    join the channel

--- js bot/index.ts
+++ js bot/index.ts
@@ -1,133 +1,153 @@
-import DiscordJS, { Guild, GuildMember, Intents, Interaction } from 'discord.js'
-import dotenv from 'dotenv'
+import DiscordJS, {
+  Guild,
+  GuildMember,
+  Intents,
+  Interaction,
+  VoiceChannel,
+} from "discord.js";
+import dotenv from "dotenv";
 import {
-	joinVoiceChannel,
-	createAudioPlayer,
-	createAudioResource,
-	entersState,
-	StreamType,
-	AudioPlayerStatus,
-	VoiceConnectionStatus,
-} from '@discordjs/voice';
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  entersState,
+  StreamType,
+  AudioPlayerStatus,
+  VoiceConnectionStatus,
+  AudioResource,
+} from "@discordjs/voice";
 
-dotenv.config()
-const client = new DiscordJS.Client(
-    {
-        intents:[
-            Intents.FLAGS.GUILDS,
-            Intents.FLAGS.GUILD_MESSAGES,
-            Intents.FLAGS.GUILD_PRESENCES,
-            Intents.FLAGS.GUILD_VOICE_STATES,
-            Intents.FLAGS.GUILD_MEMBERS,
-            Intents.FLAGS.GUILD_INTEGRATIONS
-        ]
-    }
-)
+dotenv.config();
+const client = new DiscordJS.Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_INTEGRATIONS,
+  ],
+});
 
-client.on('ready', ()=>{
-    console.log("bot is ready!")
-    let guildID:string = process.env.OLG!
-    const guild = client.guilds.cache.get(guildID)
-    let commands
-    if (guild){
-        commands = guild.commands
-    }
-    else{
-        commands = client.application?.commands
-    }
-    commands?.create({
-        name: "kamran",
-        description: "kick the nuisance!"
-    }
-    )
-})
+client.on("ready", () => {
+  console.log("bot is ready!");
+  let guildID: string = process.env.OLG!;
+  const guild = client.guilds.cache.get(guildID);
+  let commands;
+  if (guild) {
+    commands = guild.commands;
+  } else {
+    commands = client.application?.commands;
+  }
+  commands?.create({
+    name: "kamran",
+    description: "kick the nuisance!",
+  });
+});
 
-client.on('messageCreate', (message)=>{
-    if(message.content==="!kamran"){
-        message.reply({
-            content: 'soon i will kick the nuisance!'
-        })
-    }
-})
+client.on("messageCreate", (message) => {
+  if (message.content === "!kamran") {
+    message.reply({
+      content: "soon i will kick the nuisance!",
+    });
+  }
+});
 
+async function gulag(who: string, guild: DiscordJS.Guild, caller: string) {
+  const target = new Promise<string>((resolve, reject) => {
+    guild?.members.fetch(who).then((member) => {
+      resolve(member.voice.channelId as string);
+    });
+  });
+  const aimer = new Promise<string>((resolve, reject) => {
+    guild?.members.fetch(caller).then((member) => {
+      resolve(member.voice.channelId as string);
+    });
+  });
+  target.then((where) => {
+    aimer.then((wheee) => {
+      if (wheee === where) {
+        guild?.members.fetch(who).then((member) => {
+          member.voice
+            .setChannel(process.env.GULAG!)
+            .then(() => {
+              console.log("yay tranquility");
+            })
+            .catch(console.error);
+        });
+        setTimeout(() => {
+          guild?.members.fetch(who).then((member) => {
+            member.voice
+              .setChannel(where)
+              .then(() => {
+                console.log("whale cum black");
+              })
+              .catch(console.error);
+          });
+        }, 3000);
+      } else {
+        console.log("not in a same place");
+      }
+    });
+  });
+}
 
-function gulag(who:string, guild:DiscordJS.Guild, caller:string){
+async function join_and_voice(who: string, guild: DiscordJS.Guild) {
     const target = new Promise<string>((resolve, reject) => {
-        
-        guild?.members.fetch(who).then((member)=> {
-            resolve(member.voice.channelId as string);
-        })
-    });
-    const aimer = new Promise<string>((resolve, reject) => {
-        
-        guild?.members.fetch(caller).then((member)=> {
-            resolve(member.voice.channelId as string);
-        })
-    });
-    target.then(where=>{
-        aimer.then(wheee=>{
-            if(wheee===where){
-                guild?.members.fetch(who).then(member => {
-                    member.voice.setChannel(process.env.GULAG!).then(()=>{
-                        console.log("yay")
-                    }).catch(console.error)
-                })
-                setTimeout(() => {   
-                    guild?.members.fetch(who).then(member => {
-                        member.voice.setChannel(where).then(()=>{
-                            console.log("whale cum black")
-                        }).catch(console.error)
-                    })
-                }, 3000);
-            }
-            else{
-                console.log("not in a same place")
-            }
-        })
-        
-    })
-
+        guild?.members.fetch(who).then((member) => {
+          resolve(member.voice.channelId as string);
+        });
+      });
+      
+      target.then((where) => {
+        const connection = joinVoiceChannel({
+            channelId: where,
+            guildId: guild.id,
+            adapterCreator: guild.voiceAdapterCreator!,
+        });
+        const audioplayer = createAudioPlayer();
+        connection.subscribe(audioplayer);
+      })
 
 }
 
 
-
-
 client.on("interactionCreate", async (interaction) => {
-    if(!interaction.isCommand()){
-        return
-    }
-    const {commandName, options} = interaction
-    if(commandName==='kamran'){
-        interaction.reply({
-            content: "soon enough!",
-            ephemeral:true,
-        })
-        const target:string = process.env.KAMRAN!
-        gulag(process.env.TEST!, interaction.guild!, interaction.member?.user.id!)
-        
-        // const guild = client.guilds.cache.get(process.env.OLG!)
-        // const join:string = whereX(interaction.user.id)
-        // console.log(interaction.user.id)
-        // console.log(join)
-        // const connection = joinVoiceChannel({
-        //     channelId: join,
-        //     guildId: process.env.OLG!,
-        //     adapterCreator: guild?.voiceAdapterCreator!,
-        // });
-        // try{
-        //     console.log(connection.state)
-        //     await entersState(connection, VoiceConnectionStatus.Connecting, 5e3)
-        //     console.log("connecting")
-        //     await entersState(connection, VoiceConnectionStatus.Ready, 5e3)
-        // }catch(error){
-        //     connection.destroy()
-        //     console.log("couldn't find you guys")
-        //     throw error
-        // }
-        
+  if (!interaction.isCommand()) {
+    return;
+  }
+  const { commandName, options } = interaction;
+  if (commandName === "kamran") {
+    interaction.reply({
+      content: "soon enough!",
+      ephemeral: true,
+    });
+    const target: string = process.env.KAMRAN!;
+    
+    
 
-    }
-})
 
-client.login(process.env.TOKEN)
+    
+    // let resource = createAudioResource(createReadStream(join(__dirname, 'resources/try.mp3')), {
+    //     inlineVolume : true
+    // });
+    await gulag(process.env.TEST!, interaction.guild!, interaction.member?.user.id!);
+
+    await join_and_voice(process.env.TEST!, interaction.guild!)
+
+
+    
+    
+    
+
+
+    // connection.subscribe(player);
+    // player.play(resource)
+    // console.log("done");
+
+    // await interaction.reply('I have joined the voice channel!');
+  
+  }
+});
+
+client.login(process.env.TOKEN);
