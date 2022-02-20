35cf22e4cfa39d0fdb2993dc2dfc384262bca519
cybernaga <45783152+NiEmiya@users.noreply.github.com>
2022-02-20 12:05:56 -0500

    Revert "Update index.ts"
    This reverts commit a4e1f454cc8ecaeb69beb6df7bd516dd88841c4a.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { validateEnv } from "./utils/validateEnv";
 import { Client } from "discord.js";
 import { connectDatabase } from "./database/connectDatabase";
-import { onReady } from "./events/OnReady";
-import { onInteraction } from "./events/OnInteraction";
+import { onReady } from "./events/onReady";
+import { onInteraction } from "./events/onInteraction";
 import { IntentOptions } from "./config/IntentOptions";
 import dotenv from "dotenv";
 dotenv.config();
